8f105df55e5e8736763605f6eceb2f9510c604af
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2021-11-13 14:08:07 -0800

    Work on navigation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import yapLogo from './images/YapLogo.png';
 import Recorder from 'react-mp3-recorder';
 import SkynetButtons from './Components/SkynetButtons';
 import UploadArea from './Components/UploadArea';
+import Navigation from './Components/Navigation';
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
         <Recorder 
           onRecordingComplete={(blob) => {
             setBlob(blob);
+            setUploaded(false);
           }}
           onRecordingError={(err) => {
             console.log("recording error", err)
@@ -36,6 +38,7 @@ function App() {
             setUploaded={setUploaded}
           /> : ""}
         <SkynetButtons />
+        {/* <Navigation /> */}
       </div>
     </div>
   );
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -3,6 +3,7 @@ import './Navigation.css';
 function Navigation() {
   return (
     <div className="Navigation">
+      stuff
     </div>
   );
 }
