549ff9c7ce98fd6ee60764e763db48a08dd86a1a
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2021-11-13 22:57:59 -0800

    Add login to mysky button

--- src/Components/SkynetButtons.js
+++ src/Components/SkynetButtons.js
@@ -1,16 +1,36 @@
 import './SkynetButtons.css';
 import AddToHomescreen from '../images/AddToHomescreen.svg';
-// import { SkynetContext } from '../state/SkynetContext';
-// import { useContext } from 'react';
+import { SkynetContext } from '../state/SkynetContext';
+import { useContext, useEffect, useState } from 'react';
 
 function SkynetButtons() {
-    // const { mySky } = useContext(SkynetContext);
+
+    const { mySky } = useContext(SkynetContext);
+    const [ loginButton, setloginButton ] = useState("");
+
+    useEffect(() => {
+        async function stuff() {
+            try {
+                if (!await mySky.checkLogin()) {
+                    setloginButton(
+                        <button onClick={mySky.requestLoginAccess}
+                            className="login_to_mysky"
+                        >
+                            Login to mySky
+                        </button>
+                    )
+                }
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+        stuff()
+    }, [mySky])
 
     return (
         <div className="SkynetButtons">
-            {/* <button onClick={mySky.requestLoginAccess} className="login_to_mysky">
-                Login to mySky
-            </button> */}
+            {loginButton}
             <a
             target="_blank"
             href="https://homescreen.hns.siasky.net/#/skylink/AQCrn58EwIKUsZ94r9bXOGauv-KapM77N1wT62jYAXWovw"
