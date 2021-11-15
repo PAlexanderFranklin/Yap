b0449495ef19d35292a25d081b8a94ce04c55541
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2021-11-15 01:24:41 -0800

    fix wrap direction add recording timestamp

--- src/Components/Navigation/Item.js
+++ src/Components/Navigation/Item.js
@@ -1,12 +1,29 @@
 import './Item.css';
 import CopyToClipboard from 'react-copy-to-clipboard';
+import { useContext, useEffect, useState } from 'react';
+import { SkynetContext } from '../../state/SkynetContext';
 
 function Item(props) {
 
+  const { client } = useContext(SkynetContext);
   const { skylink } = props;
+  const [ fileName, setFileName ] = useState("");
+
+  useEffect(() => {
+    async function stuff() {
+      try {
+        const response = await client.getMetadata(skylink);
+        setFileName(response.metadata.filename.replace(/\.[^/.]+$/, ""));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    stuff()
+  }, [client, skylink]);
 
   return (
     <div className="Item">
+      <h4>{fileName}</h4>
       <audio src={skylink} controls="controls" preload="metadata" />
       <CopyToClipboard text={skylink}>
         <p className="UploadButton saved_item">Copy link</p>
